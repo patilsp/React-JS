4fafe5a9f482c2e04452d2b3421351e22624879a
Santosh Patil <santoshpatil4510@gmail.com>
2019-06-17 16:07:47 +0530

    kick start with hello world

---
+++ my-first-app/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  render(){
+    retrun {
+      <div className="App">
+      <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome To React</h2>
+      </div>
+    <p> className="App-intro">
+      To Get Started EDit App.js
+    </p>
+    </div>
+    };
+  }
+}
+
+class header extends Component{
+  render(){
+    return{
+      <div>
+      <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome To React</h2>
+      </div>
+
+      </div>
+    };
+  }
+}
+
+export default App;
